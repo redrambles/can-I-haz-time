f55bc74a7f73fce7e82ef189db248d321566d947
Ann Cascarano <a.cascarano@gmail.com>
2017-08-30 14:13:40 -0400

    add party time functionality

---
+++ script.js
@@ -0,0 +1,191 @@
+var time = new Date().getHours();
+var messageText;
+var noon = 12;
+var evening = 18; // 6PM
+var wakeupTime = 9; // 9AM
+var lunchTime = 12; // 12PM
+var partyTime = 17; // 5PM
+var napTime = lunchTime + 2; // 2PM
+var timeEvent = document.getElementById("timeEvent");
+var isPartyTime = false;
+var partyTimeButton = document.getElementById("partyTimeButton");
+var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
+var lolcat = document.getElementById("lolcat");
+
+var updateClock = function() {
+    if (time == partyTime){
+        messageText = "IZ PARTEE TIME!!";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
+    } else if (time == napTime) {
+        messageText = "IZ NAP TIME...";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+    } else if (time == lunchTime) {
+        messageText = "IZ NOM NOM NOM TIME!!";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+    } else if (time == wakeupTime) {
+        messageText = "IZ TIME TO GETTUP.";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+    } else if (time < noon) {
+        messageText = "Good morning!";
+    } else if (time > evening) {
+        messageText = "Good Evening!";
+    } else {
+        messageText = "Good afternoon!";
+    }
+    timeEvent.innerHTML = messageText; 
+    lolcat.src = image;
+
+    // Clock
+
+    var showCurrentTime = function()
+    {
+        // display the string on the webpage
+        var clock = document.getElementById('clock');
+    
+        var currentTime = new Date();
+    
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        var meridian = "AM";
+    
+        // Set hours 
+        if (hours >= noon) 
+        { 
+            meridian = "PM"; 
+        }  
+        if (hours > noon) 
+        { 
+            hours = hours - 12; 
+        }
+        // Set Minutes
+        if (minutes < 10)
+        {
+            minutes = "0" + minutes;
+        }
+        // Set Seconds
+        if (seconds < 10)
+        {
+            seconds = "0" + seconds;
+        }
+        // put together the string that displays the time
+        var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
+    
+        clock.innerText = clockTime;
+    };
+
+    showCurrentTime();
+};
+
+// one second
+var oneSecond = 1000;
+// Get the updateClock function to behave like a clock, by updating it every second
+setInterval(updateClock, oneSecond);
+
+updateClock();
+
+var time = new Date().getHours();
+var messageText;
+var noon = 12;
+var evening = 18; // 6PM
+var wakeupTime = 9; // 9AM
+var lunchTime = 12; // 12PM
+var partyTime = 17; // 5PM
+var napTime = lunchTime + 2; // 2PM
+var partyTimeButton = document.getElementById("partyTimeButton");
+var isPartyTime = false;
+var timeEvent = document.getElementById("timeEvent");
+var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
+var lolcat = document.getElementById("lolcat");
+
+var updateClock = function() {
+    if (time == partyTime){
+        messageText = "IZ PARTEE TIME!!";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
+    } else if (time == napTime) {
+        messageText = "IZ NAP TIME...";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+    } else if (time == lunchTime) {
+        messageText = "IZ NOM NOM NOM TIME!!";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+    } else if (time == wakeupTime) {
+        messageText = "IZ TIME TO GETTUP.";
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+    } else if (time < noon) {
+        messageText = "Good morning!";
+    } else if (time > evening) {
+        messageText = "Good Evening!";
+    } else {
+        messageText = "Good afternoon!";
+    }
+    timeEvent.innerHTML = messageText; 
+    lolcat.src = image;
+
+    // Clock
+
+    var showCurrentTime = function()
+    {
+        // display the string on the webpage
+        var clock = document.getElementById('clock');
+    
+        var currentTime = new Date();
+    
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        var meridian = "AM";
+    
+        // Set hours 
+        if (hours >= noon) 
+        { 
+            meridian = "PM"; 
+        }  
+        if (hours > noon) 
+        { 
+            hours = hours - 12; 
+        }
+        // Set Minutes
+        if (minutes < 10)
+        {
+            minutes = "0" + minutes;
+        }
+        // Set Seconds
+        if (seconds < 10)
+        {
+            seconds = "0" + seconds;
+        }
+        // put together the string that displays the time
+        var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
+    
+        clock.innerText = clockTime;
+    };
+
+    showCurrentTime();
+};
+
+// one second
+var oneSecond = 1000;
+// Get the updateClock function to behave like a clock, by updating it every second
+setInterval(updateClock, oneSecond);
+
+updateClock();
+
+// Party Time!
+var partyEvent = function() {
+   if (isPartyTime === false) {
+      isPartyTime = true;
+      time = partyTime;
+	  partyTimeButton.style.backgroundColor = "#CCC";
+      // text in the button should read "Party Over"
+      // color of the button should be "#0A8DAB" (bonus!)
+   }
+   else {
+      isPartyTime = false;
+      time = new Date().getHours();
+	   partyTimeButton.style.backgroundColor = "#222";
+      // text in the button should read "PARTY TIME!"
+      // color of the button should be "#222" (bonus!)
+   }
+};
+
+partyTimeButton.addEventListener("click", partyEvent);
